baf1ea52d2789a8b482a24d236a75b2158ffa09b
Norbert Preining <norbert@preining.info>
2024-07-01 23:39:03 +0000

    updating infra data

--- texlive-scripts/extractbb.lua
+++ texlive-scripts/extractbb.lua
@@ -42,12 +42,12 @@ local TARGET_PATH_NAME = "xdvipdfmx"
 
 -- The name to use when calling the target program. Equivalent to ``argv[0]''
 -- in C. (Example: ``extractbb'')
-local TARGET_EXEC_NAME = "ebb"
+local TARGET_EXEC_NAME = "xbb"
 
 -- Any extra arguments to be prepended to the target program, before any
 -- user-supplied arguments. Equivalent to ``argv[1], ...'' in C.
 -- (Example: ``--extractbb'')
-local TARGET_PREPEND_ARGS = { "--ebb" }
+local TARGET_PREPEND_ARGS = { "--extractbb" }
 
 -- Any extra arguments to be appended to the target program, after any
 -- user-supplied arguments. Equivalent to ``..., argv[argc]'' in C.
